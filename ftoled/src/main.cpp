5112e47fc17dfcf6aefd8bc52d5bf12040d2a35b
Grant Slender <gslender@gmail.com>
2015-10-05 14:35:05 +1000

    update for OLED display

--- ftoled/src/main.cpp
+++ ftoled/src/main.cpp
@@ -1,25 +1,58 @@
 #include "Arduino.h"
+/*
+ * Simple sketch that cycles the display through available hues,
+ * keeping a solid 100% saturation value,
+ * simulating slowly circling the HSV colour wheel.
+ */
+#include <SPI.h>
+#include <SD.h>
+#include <FTOLED.h>
 
-#define redPin   A0
-#define greenPin A1
+#include <fonts/Droid_Sans_36.h>
+#include <fonts/Droid_Sans_24.h>
+#include <fonts/Droid_Sans_16.h>
+#include <fonts/Arial_Black_16.h>
+#include <fonts/Arial14.h>
+#include <fonts/Droid_Sans_12.h>
+#include <fonts/SystemFont5x7.h>
 
-void setup()                      // run once, when the sketch starts
-{
-  pinMode(redPin, OUTPUT);        // sets the digital pin as output
-  pinMode(greenPin, OUTPUT);      // sets the digital pin as output
-  Serial.begin(115200);
+const byte pin_cs = 8;
+const byte pin_dc = 9;
+const byte pin_reset = 10;
 
+const OLED_Colour VERYDARKSLATEGRAY        = {  30/8,  30/4,  30/8 };
+
+OLED oled(pin_cs, pin_dc, pin_reset);
+//OLED_TextBox box(oled);
+
+void drawRpm(int rpm) {
+
+	oled.selectFont(Droid_Sans_36);
+	char rpmStr[6];
+	sprintf(rpmStr, "%d", rpm);
+	int w = oled.stringWidth(rpmStr);
+	byte x1 = 63-w/2;
+	byte y1=42;
+	oled.drawFilledBox(10,y1,118,y1+36,BLACK);
+	oled.drawString(x1, y1, rpmStr, ORANGERED, BLACK);
+
+}
+int rpm = 5500;
+void setup() {
+	oled.begin();
+
+	oled.drawCircle(63, 63, 63, WHITE);
+
+
+//	oled.setPixel(0,0,YELLOW);
+//	oled.setPixel(127,127,BLUE);
+	drawRpm(rpm);
 }
 
 void loop() {
 
-
-	  digitalWrite(redPin, HIGH);
-	  digitalWrite(greenPin, LOW);
-	  delay(500);
-	  digitalWrite(redPin, LOW);
-	  digitalWrite(greenPin, HIGH);
-	  delay(500);
-
+	delay(300);
+	rpm -= 15;
+	drawRpm(rpm);
 
 }
---
+++ ucglib/src/main.cpp
@@ -0,0 +1,138 @@
+/*
+ test
+ */
+
+#include <Arduino.h>
+#include <SPI.h>
+#include <Ucglib.h>
+
+Ucglib_SSD1351_18x128x128_FT_HWSPI ucg(9, 8, 10);
+
+const byte pin_cs = 8;
+const byte pin_dc = 9;
+const byte pin_reset = 10;
+
+struct Color {
+	uint8_t r;
+	uint8_t g;
+	uint8_t b;
+};
+
+char displayed_rpm_digits[5];
+
+int rpm = 1900;
+int rpmMax = 8000;
+int rpmMin = 800;
+int rpmStep = 1;
+
+int clt = 40;
+int cltMax = 120;
+int cltMin = 20;
+int cltStep = 1;
+
+inline void printCharAt(byte x, byte y, Color c, char ch) {
+	ucg.setColor(c.r, c.g, c.b);
+	ucg.setPrintPos(x, y);
+	ucg.print(ch);
+}
+
+void drawRpm(int rpm) {
+	const byte x = 20;
+	const byte y = 80;
+	const byte w = 24;
+
+	const Color fgCol = { 235, 108, 0 };
+	const Color bgCol = { 0, 0, 0 };
+
+//	ucg.setFont(ucg_font_ncenR24_tr);
+	ucg.setFont(ucg_font_fub30_tr);
+	ucg.setColor(1, bgCol.r, bgCol.g, bgCol.b);
+
+	char newRpmStr[5];
+	snprintf(newRpmStr, 5, "%4d", rpm);
+
+	for (int n = 0; n < 5; n++) {
+		if (newRpmStr[n] != displayed_rpm_digits[n]) {
+			byte wx = x + (w * n);
+			printCharAt(wx, y, bgCol, displayed_rpm_digits[n]);
+			displayed_rpm_digits[n] = newRpmStr[n];
+			printCharAt(wx, y, fgCol, displayed_rpm_digits[n]);
+		}
+	}
+}
+
+void drawClt(int clt) {
+	const byte x = 10;
+	const byte y = 95;
+	const byte w = 110;
+	const byte h = 16;
+
+	const Color bg_cold = {0,130,0};
+	const Color bg_warm = {0,0,160};
+	const Color bg_hot = {160,0,0};
+	const Color fg = {0,0,0};
+
+	ucg.setColor(0, bg_cold.r, bg_cold.g, bg_cold.b);
+	ucg.drawBox(x,y,w,h);
+
+	ucg.setFont(ucg_font_7x13_mf); // do not use _tf for UCG_FONT_MODE_SOLID
+	ucg.setFontMode(UCG_FONT_MODE_SOLID);
+	ucg.setPrintPos(x, y+11);
+	ucg.setColor(0, 0, 0, 0);
+	ucg.setColor(1, bg_cold.r, bg_cold.g, bg_cold.b);
+	ucg.printf("CLT:%d%c",clt,176); // ASCII 248 = degree symbol
+}
+
+void setup(void) {
+
+	ucg.begin(UCG_FONT_MODE_TRANSPARENT);
+
+	ucg.clearScreen();
+	ucg.setColor(25, 25, 25);
+	ucg.drawCircle(64, 64, 63, UCG_DRAW_ALL);
+
+// zero,zero is top left
+//	ucg.setColor(0, 125, 125);
+//	ucg.drawPixel(126, 126);
+//	ucg.setColor(0, 0, 125);
+//	ucg.drawPixel(2, 2);
+
+	drawRpm(0);
+	drawClt(0);
+
+	rpmMax = random(1000, 8000);
+}
+
+void loop() {
+	drawRpm(rpm);
+//	drawClt(clt);
+	delay(40);
+
+	rpm+=rpmStep;
+	if (rpmStep > 0) {
+		rpmStep = random(1, 10);
+		if (rpm > rpmMax) {
+			rpmStep = -random(1, 10);
+		}
+	} else {
+		rpmStep = -random(1, 10);
+		if (rpm < rpmMin) {
+			rpmStep = random(1, 10);
+			rpmMax = random(1000, 8000);
+		}
+	}
+
+
+	clt+=cltStep;
+	if (cltStep > 0) {
+		cltStep = random(1, 50);
+		if (clt > cltMax) {
+			cltStep = -random(1, 50);
+		}
+	} else {
+		cltStep = -random(1, 50);
+		if (clt < cltMin) {
+			cltStep = random(1, 50);
+		}
+	}
+}
